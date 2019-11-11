c684cf29beff34d37a0ef053b075e8e01e693600
Daniel Chiquito <daniel.chiquito@gmail.com>
2019-11-11 23:56:21 +0000

    Saving dummy values for nfs-env.sh

---
+++ nfs-env.sh
@@ -0,0 +1,8 @@
+
+# Update these variables as appropriate for your NFS environment
+
+export CECTF_NFS_HOST_IP="127.0.0.1"
+export CECTF_STAGING_CONFIG="/mnt/ctf-staging-config"
+export CECTF_STAGING_CHALLENGE_FILES="/mnt/ctf-staging-files"
+export CECTF_PRODUCTION_CONFIG="/mnt/ctf-production-config"
+export CECTF_PRODUCTION_CHALLENGE_FILES="/mnt/ctf-production-files"
